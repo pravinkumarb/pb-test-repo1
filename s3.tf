a3e63bc3c3d64b1f386b426469033d268c2dc1e9
pravinkumarb <pravin883@gmail.com>
2023-02-13 20:55:56 -0800

    first s3 added

---
+++ s3.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "example-bucket"
+
+  acl    = "public-read"
+
+  tags = {
+    Name = "example-bucket"
+  }
+}
